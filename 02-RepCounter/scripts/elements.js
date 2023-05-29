2395243bf6e8b08eb20dda35fb3a31a4efcde51e
Felipe Leão <felipeleao.tech@gmail.com>
2023-05-29 20:58:31 -0300

    Finalização do design do projeto e iniciacao das funcionalidades com JS

---
+++ 02-RepCounter/scripts/elements.js
@@ -0,0 +1,8 @@
+const countSeries = document.querySelector('.countSeries')
+
+countSeries.textContent = '2'
+console.log(countSeries)
+
+export const elements  = {
+  countSeries,
+}
---
+++ 02-RepCounter/scripts/index.js
@@ -0,0 +1,1 @@
+import { elements } from "./elements.js"
