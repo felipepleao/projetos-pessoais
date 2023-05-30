e1fb149577d5be86cd1b600975ee7c9a35ee897f
Felipe Leão <felipeleao.tech@gmail.com>
2023-05-29 23:49:37 -0300

    add funcionalidades de botoes de aumentar e diminuir exercicios e series

--- 02-RepCounter/scripts/elements.js
+++ 02-RepCounter/scripts/elements.js
@@ -1,8 +1,39 @@
-const countSeries = document.querySelector('.countSeries')
+const sectionSettings = document.querySelector(".section-settings")
+const sectionScore = document.querySelector(".section-score")
+const countSeries = document.querySelector(".countSeries");
+const minutesSeries = document.querySelector(".minutesSeries");
+const secondsSeries = document.querySelector(".secondsSeries");
+const countRepetitions = document.querySelector(".countRepetitions");
+const minutesRepetitions = document.querySelector(".minutesRepetitions");
+const secondsRepetitions = document.querySelector(".secondsRepetitions");
+const buttonAddSeries = document.querySelector(".timer-wrapper.One .buttonAdd");
+const buttonRemoveSeries = document.querySelector(".timer-wrapper.One .buttonRemove");
+const buttonAddTimeSeries = document.querySelector(".timer-wrapper.Two .buttonAdd");
+const buttonRemoveTimeSeries = document.querySelector(".timer-wrapper.Two .buttonRemove");
+const buttonAddRepetitions = document.querySelector(".timer-wrapper.Three .buttonAdd");
+const buttonRemoveRepetitions = document.querySelector(".timer-wrapper.Three .buttonRemove");
+const buttonAddTimeRepetitions = document.querySelector(".timer-wrapper.Four .buttonAdd");
+const buttonRemoveTimeRepetitions = document.querySelector(".timer-wrapper.Four .buttonRemove");
+const buttonStart = document.querySelector(".button-config")
+const buttonReset = document.querySelector(".button-cancel")
 
-countSeries.textContent = '2'
-console.log(countSeries)
-
-export const elements  = {
+export const elements = {
+  sectionSettings,
+  sectionScore,
   countSeries,
-}
+  minutesSeries,
+  secondsSeries,
+  countRepetitions,
+  minutesRepetitions,
+  secondsRepetitions,
+  buttonAddSeries,
+  buttonAddTimeSeries,
+  buttonRemoveTimeSeries,
+  buttonRemoveSeries,
+  buttonAddRepetitions,
+  buttonRemoveRepetitions,
+  buttonAddTimeRepetitions,
+  buttonRemoveTimeRepetitions,
+  buttonStart,
+  buttonReset,
+};
--- 02-RepCounter/scripts/index.js
+++ 02-RepCounter/scripts/index.js
@@ -1,1 +1,137 @@
-import { elements } from "./elements.js"
+import { elements } from "./elements.js";
+
+const {
+  sectionSettings,
+  sectionScore,
+  countSeries,
+  minutesSeries,
+  secondsSeries,
+  countRepetitions,
+  minutesRepetitions,
+  secondsRepetitions,
+  buttonAddSeries,
+  buttonAddTimeSeries,
+  buttonRemoveTimeSeries,
+  buttonRemoveSeries,
+  buttonAddRepetitions,
+  buttonRemoveRepetitions,
+  buttonAddTimeRepetitions,
+  buttonRemoveTimeRepetitions,
+  buttonStart,
+  buttonReset,
+} = elements;
+
+console.log(buttonStart);
+
+//events
+buttonAddSeries.addEventListener("click", addSeries);
+buttonRemoveSeries.addEventListener("click", removeSeries);
+buttonAddTimeSeries.addEventListener("click", addTimeSeries);
+buttonRemoveTimeSeries.addEventListener("click", removeTimeSeries);
+buttonAddRepetitions.addEventListener("click", addRepetitions);
+buttonRemoveRepetitions.addEventListener("click", removeRepetitions);
+buttonAddTimeRepetitions.addEventListener("click", addTimeRepetitions);
+buttonRemoveTimeRepetitions.addEventListener("click", removeTimeRepetitions);
+buttonStart.addEventListener("click", startSettings);
+buttonReset.addEventListener("click", resetSettings)
+
+//functions
+function addSeries() {
+  let series = parseInt(countSeries.textContent);
+  series++;
+  countSeries.textContent = String(series).padStart(2, "0");
+}
+
+function removeSeries() {
+  let series = parseInt(countSeries.textContent);
+  --series;
+  series > 0
+    ? (countSeries.textContent = String(series).padStart(2, "0"))
+    : null;
+}
+
+function addTimeSeries() {
+  let minutes = Number(minutesSeries.textContent);
+  let seconds = Number(secondsSeries.textContent);
+  seconds += 30;
+
+  if (seconds < 60) {
+    secondsSeries.textContent = seconds;
+  } else if (minutes < 9) {
+    secondsSeries.textContent = "00";
+    minutes++;
+    minutesSeries.textContent = minutes;
+  }
+}
+
+function removeTimeSeries() {
+  let minutes = Number(minutesSeries.textContent);
+  let seconds = Number(secondsSeries.textContent);
+
+  if (minutes > 0) {
+    seconds -= 30;
+    if (seconds < 0) {
+      seconds = 30;
+      --minutes;
+      minutesSeries.textContent = minutes;
+    }
+    secondsSeries.textContent = String(seconds).padStart(2, "0");
+  }
+}
+
+function addRepetitions() {
+  let repetitions = parseInt(countRepetitions.textContent);
+  repetitions++;
+  countRepetitions.textContent = String(repetitions).padStart(2, "0");
+}
+
+function removeRepetitions() {
+  let repetitions = parseInt(countRepetitions.textContent);
+  --repetitions;
+  repetitions > 0
+    ? (countRepetitions.textContent = String(repetitions).padStart(2, "0"))
+    : null;
+}
+
+function addTimeRepetitions() {
+  let minutes = Number(minutesRepetitions.textContent);
+  let seconds = Number(secondsRepetitions.textContent);
+  seconds += 30;
+
+  if (seconds < 60) {
+    secondsRepetitions.textContent = seconds;
+  } else if (minutes < 9) {
+    secondsRepetitions.textContent = "00";
+    minutes++;
+    minutesRepetitions.textContent = minutes;
+  }
+}
+
+function removeTimeRepetitions() {
+  let minutes = Number(minutesRepetitions.textContent);
+  let seconds = Number(secondsRepetitions.textContent);
+
+  if (minutes > 0) {
+    seconds -= 30;
+    if (seconds < 0) {
+      seconds = 30;
+      --minutes;
+      minutesRepetitions.textContent = minutes;
+    }
+    secondsRepetitions.textContent = String(seconds).padStart(2, "0");
+  }
+}
+
+function startSettings() {
+  toggleCard()
+}
+
+function resetSettings() {
+  
+  toggleCard()
+}
+
+function toggleCard() {
+  sectionSettings.classList.toggle("hidden")
+  sectionScore.classList.toggle("hidden")
+}
